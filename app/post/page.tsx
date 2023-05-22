a17da3c6cd71615d1d8f4383c3d328720a1cfd28
lhn <1498613780@qq.com>
2023-05-22 21:02:12 +0800

    works

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -4,7 +4,7 @@ const Index = () => {
   return (
     <div className="px-16 py-8 w-full max-w-screen-md h-full min-h-[calc(100vh-theme(height.navh))]">
       <div className="h-2/3 flex justify-center items-center">
-        <div className="text-rose-600 text-4xl font-bold">POST</div>
+        <div className="text-4xl font-bold">POST</div>
       </div>
     </div>
   );
