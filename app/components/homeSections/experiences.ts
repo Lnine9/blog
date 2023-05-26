e6960e7d3ef42ee9c4194802e30021276644d1c4
lhn <1498613780@qq.com>
2023-05-26 20:12:20 +0800

    works

--- app/components/homeSections/experiences.ts
+++ app/components/homeSections/experiences.ts
@@ -14,26 +14,26 @@ export const experiences: Experience[] = [
   },
   {
     date: "2021/06",
-    title: "团队",
+    title: "加入团队",
     img: "https://cdn.lnine9.icu/images/experience/1684336381233.jpg?imageView2/0/interlace/1/q/50|imageslim",
     desc: "进入鹰眼团队实验室，并参与项目开发。生活开始忙碌但充实，学到了许多技术，了解到软件开发的流程。",
   },
   {
     date: "2022/05",
-    title: "玉玉",
+    title: "玉玉了",
     img: "https://cdn.lnine9.icu/images/experience/1684336249956.jpg?imageView2/0/interlace/1/q/50|imageslim",
-    desc: "持续久坐学习与工作导致腰伤，卧床几个月，抑郁了许久。持续疼痛近一年，无法正常生活、学习。人生最低谷",
+    desc: "受傷，持续疼痛，连续卧床了几个月，感觉人生到了最低谷。",
   },
   {
     date: "2022/08",
     title: "坚持",
     img: "https://cdn.lnine9.icu/images/experience/1684336691535.jpg?imageView2/0/interlace/1/q/50|imageslim",
-    desc: "同时负责两个项目，带着伤病在宿舍趴着工作，虽然疼痛但却是最充实的时光。两个月的坚持，顺利完成项目交接。",
+    desc: "同时负责两个项目，带着伤病在宿舍趴着工作，虽然疼痛但还是蛮充实。这样坚持了一个月，顺利完成项目交接。",
   },
   {
     date: "2023/05",
     title: "转变",
     img: "https://cdn.lnine9.icu/images/experience/1684336224246.jpg?imageView2/0/interlace/1/q/50|imageslim",
-    desc: "伤病后人生观念转变了许多，健康才是一切。一年的时间里坚持锻炼、自律生活，伤病稳定了许多，精神状态比之前更好了。",
+    desc: "健康才是一切，一年的时间里坚持锻炼、自律生活，伤病稳定了许多，精神状态比之前更好了。",
   },
 ];
